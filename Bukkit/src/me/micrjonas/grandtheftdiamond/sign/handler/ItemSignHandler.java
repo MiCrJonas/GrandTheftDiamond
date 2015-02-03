b71a4492c3fc8ff5996e49e0b4db6aeab0c8b901
MiCrJonas <jonas.tennie@gmx.de>
2015-02-03 17:22:12 +0100

    Created class

---
+++ Bukkit/src/me/micrjonas/grandtheftdiamond/sign/handler/ItemSignHandler.java
@@ -0,0 +1,23 @@
+package me.micrjonas.grandtheftdiamond.sign.handler;
+
+import org.bukkit.entity.Player;
+
+
+/**
+ * Listens to the interaction of a {@link Player} with an item sign
+ */
+public class ItemSignHandler implements SignHandler {
+
+	@Override
+	public void signClicked(Player clicker, String[] lines, String[] parsedLines) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isValid(String[] lines, String[] parsedLines) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
