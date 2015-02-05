f3834b0ec57a12f84308e75932939233afda85cc
MiCrJonas <jonas.tennie@gmx.de>
2015-02-05 20:04:23 +0100

    Implemented method signClicked()

--- Bukkit/src/me/micrjonas/grandtheftdiamond/sign/handler/JoinSignHandler.java
+++ Bukkit/src/me/micrjonas/grandtheftdiamond/sign/handler/JoinSignHandler.java
@@ -1,5 +1,13 @@
 package me.micrjonas.grandtheftdiamond.sign.handler;
 
+import me.micrjonas.grandtheftdiamond.GameManager;
+import me.micrjonas.grandtheftdiamond.GrandTheftDiamond;
+import me.micrjonas.grandtheftdiamond.Team;
+import me.micrjonas.grandtheftdiamond.api.event.cause.JoinReason;
+import me.micrjonas.grandtheftdiamond.data.TemporaryPluginData;
+import me.micrjonas.grandtheftdiamond.messenger.Messenger;
+import me.micrjonas.grandtheftdiamond.messenger.NoPermissionType;
+
 import org.bukkit.entity.Player;
 
 /**
@@ -9,8 +17,21 @@ public class JoinSignHandler implements SignHandler {
 
 	@Override
 	public void signClicked(Player clicker, String[] lines, String[] parsedLines) {
-		// TODO Auto-generated method stub
-		
+		if (TemporaryPluginData.getInstance().isIngame(clicker)) {
+			Messenger.getInstance().sendPluginMessage(clicker, "alreadyIngame");
+			return;
+		}
+		if (lines[1].contains("cop")) {
+			if (GrandTheftDiamond.checkPermission(clicker, "use.sign.join.cop", true, NoPermissionType.USE)) {
+				GameManager.getInstance().joinGame(clicker, Team.COP, JoinReason.SIGN);
+			}
+		}
+			
+		else if (lines[1].contains("civilian")) {
+			if (GrandTheftDiamond.checkPermission(clicker, "use.sign.join.civilian", true, NoPermissionType.USE)) {
+				GameManager.getInstance().joinGame(clicker, Team.CIVILIAN, JoinReason.SIGN);
+			}
+		}
 	}
 
 	@Override
